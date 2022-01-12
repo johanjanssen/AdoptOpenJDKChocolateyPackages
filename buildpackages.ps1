3a64be6b7662712c0098dba29a1c46eafadea2c6
Johan Janssen <johanjanssen2001@gmail.com>
2022-01-12 18:03:47 +0100

    Added configuration to deprecate the AdoptOpenJDK packages and automatically install Adoptium Temurin instead.

---
+++ buildpackages.ps1
@@ -0,0 +1,6 @@
+choco pack AdoptOpenJDK.nuspec
+choco pack AdoptOpenJDKjre.nuspec
+choco pack AdoptOpenJDK11.nuspec
+choco pack AdoptOpenJDK11jre.nuspec
+choco pack AdoptOpenJDK8.nuspec
+choco pack AdoptOpenJDK8jre.nuspec
